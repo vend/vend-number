be8d1960c623ccbea04ca2ab0f6cd9b666c4df09
Aarron Painter <aarron.painter@gmail.com>
2023-11-06 17:12:36 +1300

    Use defined ROUNDING_MODES within VendNumber

--- index.d.ts
+++ index.d.ts
@@ -57,17 +57,7 @@ declare module 'vend-number' {
   export default class VendNumber extends BigNumber {
     constructor (value?: Stringable)
 
-    static readonly ROUNDING_MODES: {
-      ROUND_UP: RoundingMode,
-      ROUND_DOWN: RoundingMode,
-      ROUND_CEIL: RoundingMode,
-      ROUND_FLOOR: RoundingMode,
-      ROUND_HALF_UP: RoundingMode,
-      ROUND_HALF_DOWN: RoundingMode,
-      ROUND_HALF_EVEN: RoundingMode,
-      ROUND_HALF_CEIL: RoundingMode,
-      ROUND_HALF_FLOOR: RoundingMode
-    }
+    static readonly ROUNDING_MODES: typeof ROUNDING_MODES
 
     static vn(value?: Stringable): VendNumber
     static round(value?: Stringable, decimalPoints?: number, roundingMode?: RoundingMode): string
